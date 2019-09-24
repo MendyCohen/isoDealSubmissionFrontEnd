b4f591055919339c7738a9e37a2da24eddc3f363
mendy cohen <mendycohen@mendys-MacBook-Pro.local>
2019-09-24 17:58:38 -0400

    First commit

---
+++ isoDealsSubmissionFrontEnd.js
@@ -0,0 +1,25 @@
+////////////////Page One////////////////////////
+function submitNewDeal() {
+  location.href = 'file:///Users/mendycohen/programs/isoDealsSubmissionFrontEnd/isoDealsSubmissionFrontEndP2.html';
+}
+
+function submitNewDealMiddleBtn() {
+  alert('Plugin functionality');
+}
+
+//////////////Page Two///////////////////////////
+function backBtnPage2() {
+  location.href = 'file:///Users/mendycohen/programs/isoDealsSubmissionFrontEnd/isoDealsSubmissionFrontEndP1.html';
+}
+
+function page2Btn() {
+  location.href = 'file:///Users/mendycohen/programs/isoDealsSubmissionFrontEnd/isoDealsSubmissionFrontEndP3.html';
+}
+/////////////Page Three////////////////////////
+function backBtnPage3() {
+  location.href = 'file:///Users/mendycohen/programs/isoDealsSubmissionFrontEnd/isoDealsSubmissionFrontEndP2.html';
+}
+
+function page3Btn() {
+  location.href = 'file:///Users/mendycohen/programs/isoDealsSubmissionFrontEnd/isoDealsSubmissionFrontEndP4.html';
+}
